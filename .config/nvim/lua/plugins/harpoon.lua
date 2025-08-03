90e59e15eb8e6cf53439a3a2587173cd4bdf7dcf
Austin Carter <austinbennettcarter@gmail.com>
2025-08-03 07:27:16 -0600

    Updating personal config

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,8 +7,8 @@ return {
         vim.keymap.set("n", "<leader>a", mark.add_file)
         vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-        vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "<C-y>", function() ui.nav_file(2) end)
         local harpoon = require('harpoon')
         harpoon.setup({
             menu = {
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,9 +2,21 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            --require("lspconfig").clangd.setup {
             --    single_file_support = true,
+            --require("lspconfig").clangd.setup {
             --}
+            require("lspconfig").clangd.setup {
+                -- https://github.com/p00f/clangd_extensions.nvim
+                cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+                init_options = {
+                    fallbackFlags = { '-std=c++17', '-DRDIPF_lnx64' },
+                },
+                on_attach = function(_, buf) 
+                    -- force the sign column to stay open for icons
+                    -- https://neovim.discourse.group/t/can-i-force-the-diagnostics-gutter-to-stay-open/1269/4
+                    vim.opt.signcolumn = "yes"
+                end
+            }
         end,
     }
 }

7215d2bf4ed040b2065201aad8b4336d36d861cf
Austin Carter <austinbennettcarter@gmail.com>
2025-09-07 05:01:13 -0600

    Added lsp proper go to definition

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,7 @@ return {
             --}
             require("lspconfig").clangd.setup {
                 -- https://github.com/p00f/clangd_extensions.nvim
+                -- cmd = {'clangd', '--background-index', '--clang-tidy', '--config=local', '--log=verbose'},
                 cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
                 init_options = {
                     fallbackFlags = { '-std=c++17', '-DRDIPF_lnx64' },
@@ -15,6 +16,9 @@ return {
                     -- force the sign column to stay open for icons
                     -- https://neovim.discourse.group/t/can-i-force-the-diagnostics-gutter-to-stay-open/1269/4
                     vim.opt.signcolumn = "yes"
+
+                    -- Set up key mapping for 'Go to Definition'
+                    vim.api.nvim_buf_set_keymap(buf, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
                 end
             }
         end,
